31b88a0d99d20363fb8ea9181896d279cbd5950e
Saranga Abeykoon <amisaranga@gmail.com>
2021-08-31 21:10:47 +0530

    Common HTML content moved to common files

---
+++ about.php
@@ -0,0 +1,6 @@
+<?php include('./includes/top.php'); ?>
+
+<h1>About Us</h1>
+<p>The introduction to the organization.</p>
+
+<?php include('./includes/bottom.php'); ?>
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title>Quick Boilerplate - PHP</title>
-    </head>
-    <body>
-        <h1>Quick Boilerplate</h1>
-    </body>
-</html>
+<?php include('./includes/top.php'); ?>
+<?php include('./includes/nav.php'); ?>
+<div>
+    <h1>Quick Boilerplate</h1>
+</div>
+<?php include('./includes/footer.php'); ?>
+<?php include('./includes/bottom.php'); ?>
